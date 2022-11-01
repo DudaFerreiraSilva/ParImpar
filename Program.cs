259590d7e9140bd621c412951f870d1d746d00ba
Eduarda Ferreira <edududaferreira10@gmail.com>
2022-11-01 19:49:16 -0300

    Resolução

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int numero;
+string paridade;
+
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.Write("Digite um número: ");
+Console.ResetColor();
+
+numero = Convert.ToInt32(Console.ReadLine());
+
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+paridade = (numero % 2) == 0 ? "par" : "ímpar";
+
+Console.WriteLine($"{numero} é {paridade}");
+Console.ResetColor();
